7f1c865119070fb142e12bef38e8918acbd3f113
KrolSeb <Sebastian.Krol@Live.umcs.edu.pl>
2019-12-17 17:09:20 +0100

    Fixed code to work properly on different compilers and environments. Added my configuration file - CMakeLists.txt.

--- main.c
+++ main.c
@@ -19,12 +19,11 @@ int integerToBinary(int num) {
     return (num % 2) + 10 * integerToBinary(num / 2);
 }
 
-int calculateBaseVectorCount(double probability[],int regSize){
-
+int calculateBaseVectorCount(double probability[], int regSize) {
     int count = 0;
 
-    for(int i = 0 ; i < (int)pow(regSize,2); i++) {
-        if((double)probability[i] != 0.0) {
+    for (int i = 0; i < (int) pow(regSize, 2); i++) {
+        if ((double) probability[i] != 0.0) {
             count++;
         }
     }
@@ -32,42 +31,39 @@ int calculateBaseVectorCount(double probability[],int regSize){
     return count;
 }
 
-void validateArraySize(double probability[],int regSize) {
-
-
-    if (regSize<1) {
+void validateArraySize(double probability[], int regSize) {
+    if (regSize < 1) {
         printf("[ERROR] Register size cannot be less than 1");
         exit(1);
     }
 
-    int n = sizeof(probability);
-    if(n != pow(regSize,2)){
+    int n = sizeof((int) probability);
+    if (n != pow(regSize, 2)) {
         printf("[ERROR] Invalid Register arr with []Probability");
         exit(1);
     }
 
 }
 
-void validateProbability(double probability[],int regSize){
+void validateProbability(double probability[], int regSize) {
     double result = 0.0;
 
-    for(int i = 0 ; i < (int)pow(regSize,2); i++) {
-        result += pow(fabs(probability[i]),2);
+    for (int i = 0; i < (int) pow(regSize, 2); i++) {
+        result += pow(sqrt(fabs(probability[i])), 2);
     }
 
-    if(result != 1.0) {
-        printf("[ERROR] Probability schould be equal 1");
+    if (result != 1.0) {
+        printf("[ERROR] Probability should be equal 1");
         exit(1);
     }
 }
 
 struct QuantumComputer initialQuantumRegisters(int regSize, double probability[]) {
-
-    validateArraySize(probability,regSize);
-    validateProbability(probability,regSize);
+    validateArraySize(probability, regSize);
+    validateProbability(probability, regSize);
 
     struct QuantumComputer quantumComputer = {
-            .baseVectorsCount = calculateBaseVectorCount(probability,regSize),
+            .baseVectorsCount = calculateBaseVectorCount(probability, regSize),
             .registerSize = regSize,
             .probability = probability
     };
@@ -77,7 +73,7 @@ struct QuantumComputer initialQuantumRegisters(int regSize, double probability[]
 
 void viewProbability(struct QuantumComputer *qc) {
     printf("\nVector with probabilities {");
-    for(int i = 0 ; i < (int)pow(qc->registerSize,2); i++) {
+    for (int i = 0; i < (int) pow(qc->registerSize, 2); i++) {
         printf("[%.4f]", qc->probability[i]);
     }
     printf("}\n");
@@ -86,19 +82,17 @@ void viewProbability(struct QuantumComputer *qc) {
 void viewQubitInMathExpression(struct QuantumComputer *qc) {
     printf("\nQubit in expression: ");
 
-    for(int i = 0 ; i < (int)pow(qc->registerSize,2); i++) {
-        if(qc->probability[i] != 0) {
-            printf("%.4f|%d> ",qc->probability[i],integerToBinary(i));
-
+    for (int i = 0; i < (int) pow(qc->registerSize, 2); i++) {
+        if (qc->probability[i] != 0) {
+            printf("%.4f|%d> ", qc->probability[i], integerToBinary(i));
         }
     }
 }
 
 int main() {
-
     int registerSize = 2;
-    double probability[] = {0,0,1,0};
-    struct QuantumComputer qc = initialQuantumRegisters(registerSize,&probability);
+    double probability[] = {0, 0, 1, 0};
+    struct QuantumComputer qc = initialQuantumRegisters(registerSize, probability);
     viewQubitInMathExpression(&qc);
     viewProbability(&qc);
     return 0;
