94530d860d9f93c1530f02d6afe10dac5ade0b32
Lukasz Kalicki <lukasz.kalicki@contractors.roche.com>
2020-01-28 12:28:06 +0100

    refactor - move to vectors

--- lib/quantum.cpp
+++ lib/quantum.cpp
@@ -1,110 +1,131 @@
 #include "quantum.h"
 #include <math.h>
 #include <iostream>
+#include <vector>
 
 struct QuantumComputer {
     int registerSize;
-    double *probability;
     int baseVectorsCount;
-};
+    bool isNormalize;
+    bool isMeasured ;
+    std::vector<double> baseVector;
 
-int integerToBinary(int num) {
-    if (num == 0) {
-        return 0;
-    }
-    if (num == 1) {
-        return 1;
-    }
-    return (num % 2) + 10 * integerToBinary(num / 2);
-}
+    QuantumComputer(int regSize, double probability[],int arrSize){
+        this->registerSize = regSize;
+        this->baseVectorsCount = 0;
+        this->isNormalize = false;
+        this->isMeasured = false;
 
-int calculateBaseVectorCount(double probability[], int regSize) {
-    int count = 0;
+        validateArraySize(arrSize, regSize);
 
-    for (int i = 0; i < (int) pow(regSize, 2); i++) {
-        if ((double) probability[i] != 0.0) {
-            count++;
-        }
-    }
+        for(int i =0 ; i< arrSize; i++){
+            baseVector.push_back(probability[i]);
+        };
 
-    return count;
-}
+        validateProbability();
+        countNonZeroBaseVector();
 
-void validateArraySize(int arrSize, int regSize) {
-    if (regSize < 1) {
-        printf("[ERROR] Register size cannot be less than 1");
-        exit(1);
-    }
-
-    if (arrSize != pow(regSize, 2)) {
-        printf("[ERROR] Invalid Register arr with []Probability");
-        exit(1);
-    }
-}
-
-void validateProbability(double probability[], int regSize) {
-    double result = 0.0;
-
-    for (int i = 0; i < (int) pow(regSize, 2); i++) {
-        result += pow(sqrt(fabs(probability[i])), 2);
-    }
-
-    if (result != 1.0) {
-        printf("[ERROR] Probability should be equal 1");
-        exit(1);
-    }
-}
-
-struct QuantumComputer initialQuantumRegisters(int regSize, double probability[],int arrSize) {
-    validateArraySize(arrSize, regSize);
-    validateProbability(probability, regSize);
-
-    struct QuantumComputer quantumComputer = {
-            .registerSize = regSize,
-            .probability = probability,
-            .baseVectorsCount = calculateBaseVectorCount(probability, regSize)
     };
-
-    return quantumComputer;
-}
-
-void resetRegister(struct QuantumComputer *qc) {
-    for (int i = 0; i < (int) pow(qc->registerSize, 2); i++) {
-        qc->probability[i] = 0.0;
-    };
-
-    qc->probability[0] = 1.0;
-}
-
-void viewProbability(struct QuantumComputer *qc) {
-    std::cout << "\nVector with probabilities {";
-    for (int i = 0; i < (int) pow(qc->registerSize, 2); i++) {
-        printf("[%.4f]", qc->probability[i]);
-    }
-    std::cout << "}\n";
-}
-
-void viewQubitInMathExpression(struct QuantumComputer *qc) {
-    printf("\nQubit in expression: ");
-
-    for (int i = 0; i < (int) pow(qc->registerSize, 2); i++) {
-        if (qc->probability[i] != 0) {
-            if(qc->probability[i] == 1.00) {
-                printf("|%d> ", integerToBinary(i));
-            } else {
-                printf("%.4f |%d> ", qc->probability[i], integerToBinary(i));
+    void countNonZeroBaseVector() {
+        for(auto x: this->baseVector){
+            if ((double) x != 0.0) {
+                this->baseVectorsCount++;
             }
         }
     }
-}
+
+    void resetState() {
+        std::vector<double> :: iterator it;
+        for(it = this->baseVector.begin(); it< this->baseVector.end(); it++) {
+            *it = 0.0;
+        }
+        this->baseVector.at(0) = 1;
+    }
+
+    static int integerToBinary(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        if (num == 1) {
+            return 1;
+        }
+        return (num % 2) + 10 * integerToBinary(num / 2);
+    }
+
+    void viewProbability() {
+        std::cout << "\nVector with probabilities {";
+        for (auto x: this->baseVector) {
+            printf("[%.4f]", x);
+        }
+        std::cout << "}\n";
+    }
+
+    void viewQubitInMathExpression() {
+        if(!this->isNormalize){
+            std::cout<<"Base Vector is not in normalize state. To view qubit you should normalize it before.  Use .normalize() function for that"<<std::endl;
+        }
+
+        else {
+            printf("\nQubit in expression: ");
+            int i = 0;
+            for (auto x: this->baseVector) {
+                if (x != 0) {
+                    int binary = integerToBinary(i);
+//                    std::string binaryStringValue = std::to_string(binary);  TODO: Refactor printing of qubit in qxpression  for register(2) -> |00> not |0>
+                    if (x == 1.00) {
+                        printf("|%d> ", binary);
+                    } else {
+                        printf("%.4f |%d> ", x, binary);
+                    }
+                }
+
+                i++;
+            }
+        }
+    }
+
+    void validateArraySize(int arrSize, int regSize) {
+        if (regSize < 1) {
+            printf("[ERROR] Register size cannot be less than 1");
+            exit(1);
+        }
+
+        if (arrSize != pow(regSize, 2)) {
+            printf("[ERROR] Invalid Register arr with []Probability");
+            exit(1);
+        }
+    }
+
+    void validateProbability() {
+        double result = 0.0;
+
+        for(auto x: this->baseVector){
+            result += pow(sqrt(fabs(x)),2);
+        }
+
+        if (result != 1.0) {
+            printf("[ERROR] Probability should be equal 1, u can normalize vector use .normalize() function for that");
+            this->isNormalize = false;
+        } else {
+            this->isNormalize = true;
+        }
+
+    }
+
+    void normalizeRegister(){
+        std::cout<<"[Here will be function to normalize vector!]"<<std::endl;
+    }
+};
+
 
 void quantum::simulate() {
     int registerSize = 2;
     double probability[] = {0.0, 0.0, 1.0, 0.0 };
     auto arrSize = std::size(probability);
-    struct QuantumComputer qc = initialQuantumRegisters(registerSize, probability,arrSize);
-    viewQubitInMathExpression(&qc);
-    viewProbability(&qc);
-    resetRegister(&qc);
-    viewProbability(&qc);
+    struct QuantumComputer qc =  QuantumComputer(registerSize, probability,arrSize);
+
+
+    qc.resetState();
+    qc.viewProbability();
+    qc.viewQubitInMathExpression();
 }
